d78a4b84962805a5247b6b00058db987f7fc5371
xun <dabby6521@gmail.com>
2021-12-15 07:44:10 +0800

    20211215

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="add">
+      <input type="text" v-model="form.account" placeholder="帳號">
+      <input type="password" v-model="form.password" placeholder="密碼">
+      <input type="number" v-model="form.age" placeholder="年齡">
+      <input type="text" v-model="form.email" placeholder="信箱">
+      <input type="submit">
+    </form>
+    <hr>
+    <table border="1">
+        <tr v-for="(user, idx) in users" :key="user._id">
+          <td>{{ user.account }}</td>
+          <td>{{ user.age }}</td>
+          <td>{{ user.email }}</td>
+          <td>
+            <input type="button" value="編輯">
+            <input type="button" value="刪除" @click="del(idx)">
+          </td>
+        </tr>
+      </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      users: [],
+      form: {
+        account: '',
+        password: '',
+        age: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    async del (idx) {
+      try {
+        await this.axios.delete('/' + this.users[idx]._id)
+        this.users.splice(idx, 1)
+      } catch (error) {
+        alert('刪除失敗')
+      }
+    },
+    async add () {
+      try {
+        const { data } = await this.axios.post('/', this.form)
+        this.users.push(data.result)
+      } catch (error) {
+        alert(error.response.data.message)
+      }
+    }
+  },
+  async created () {
+    try {
+      const { data } = await this.axios.get('/')
+      this.users = data.result
+    } catch (error) {
+      console.log(error)
+      alert('取得資料失敗')
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+axios.defaults.baseURL = 'http://localhost:3000'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
